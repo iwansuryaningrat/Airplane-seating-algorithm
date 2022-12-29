e6bf84da2cfa46ac70e2deca3220bc14c1c06c58
iwansuryaningrat <iwan.suryaningrat28@gmail.com>
2022-12-29 20:20:01 +0700

    Create Constructor

---
+++ js/script.js
@@ -0,0 +1,7 @@
+function Seat(block, row, column, classSeat, passenger) {
+  this.block = block;
+  this.row = row;
+  this.column = column;
+  this.classSeat = classSeat;
+  this.passenger = passenger;
+}

0ca0586a67a5f9812ae8b4a19efe3426a2ac1266
iwansuryaningrat <iwan.suryaningrat28@gmail.com>
2022-12-29 20:24:21 +0700

    Create Initialization

--- js/script.js
+++ js/script.js
@@ -5,3 +5,35 @@ function Seat(block, row, column, classSeat, passenger) {
   this.classSeat = classSeat;
   this.passenger = passenger;
 }
+
+var controller = {
+  // Initialization
+  init: function () {
+    let seats = document.getElementById("seats");
+    let row, column, block, newSeat, i, j, z;
+    let table, tr, td;
+    let result = [];
+
+    document.getElementById("button").addEventListener("click", function () {
+      let queue = document.getElementById("queue").value;
+      let stringRowsColumns = document.getElementById("rowsColumns").value;
+      let inputArrayRowsColumns = controller.parseInput(stringRowsColumns);
+      result = [];
+
+      view.clearFromDOM(seats); // removing old result
+
+      if (controller.isInputValid(inputArrayRowsColumns, queue) === false) {
+        return false;
+      }
+
+      view.addBackgroundImg();
+      controller.sortSeat(inputArrayRowsColumns, result);
+      result.sort(controller.comparator("column"));
+      result.sort(controller.comparator("classSeat"));
+
+      controller.seatPassengers(result, queue);
+      result.sort(controller.comparator("row", "column", "block"));
+      view.createTableResults(inputArrayRowsColumns, result);
+    });
+  },
+};
